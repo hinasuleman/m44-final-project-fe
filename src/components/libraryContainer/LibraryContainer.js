c3cb8c546015d245720c3d93a27cfb9725c7a1ba
hinasuleman <119439264+hinasuleman@users.noreply.github.com>
2023-02-10 16:43:54 +0000

    library component updated

---
+++ src/components/libraryContainer/LibraryContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import LibraryCard from "";
+import './LibraryContainer.css';
+
+const LibraryContainer = () => {
+  
+    return (
+      <div className="LibraryContainer">
+     <p>Library container</p>
+      </div>
+    )
+    }
+
+
+export default LibraryContainer;
