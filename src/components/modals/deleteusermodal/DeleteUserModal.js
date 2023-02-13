914543f0f2394f6ebe3587d9ae85fd095f729dde
hinasuleman <119439264+hinasuleman@users.noreply.github.com>
2023-02-13 17:51:08 +0000

    delete user profile modal created

--- src/components/modals/deleteusermodal/DeleteUserModal.js
+++ src/components/modals/deleteusermodal/DeleteUserModal.js
@@ -1,44 +1,19 @@
 import React from "react"
-import "./DeleteUserModal.css";
 import { useState } from "react";
 import {Modal, Button} from "react";
 
-export default function UserprofilePage() {
-    const [currentEmail, setCurrentEmail] = useState("")
-    const [currentPassword, setCurrentPassword] = useState("")
-
-    function changehandler(event, setter, state) {
-        setter(event.target.value)
-        console.log(state)
-    }
-    function submitHandler1(event) {
-      event.preventDefault()
-      console.log("userprofile functionality");
+const DeleteUserModal = () => {
+      console.log("deleteuser functionality");
     }
     return (
-      <div className="userprofile"> 
-          {/* imagetag */}
+      <div className="deleteuserprofile"> 
         <form onSubmit={(event) => submitHandler1(event)}>
           <input placeholder="current email" onChange={(event) => changehandler(event,setCurrentEmail,currentEmail)} />
           <input placeholder= "current password" onChange={(event) => changehandler(event,setCurrentPassword,currentPassword)} />
           </form>
           <br></br>
-
-const DeleteConfirmation = ({showModal, hideModal, confirmModal, id, type, message}) => {
-    return (
-        <Modal show={showModal} onHide={hideModal}>
-        <Modal.Header closeButton>
-            <Modal.Title>Delete Confirmation</Modal.Title>
-        </Modal.Header>
-        <Modal.Body><div className="alert alert-danger">{Are you sure you want to delete your account? Enter details to confirm}
-</div></Modal.Body>
-        <Modal.Footer>
-            <Button variant="danger" onClick={() => confirmModal (type, id) }>
-                Delete
-            </Button>
-        </Modal.Footer>
-    </Modal>
+          <button>Delete user</button>
+          </div>
     )
-}
-
-export default DeleteConfirmation;
+    
+export default DeleteUserModal;
