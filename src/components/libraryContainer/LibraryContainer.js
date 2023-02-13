d05e4c8e287d481bebb21a840204c88ef4f93975
Chris Sutcliffe <cjsutcliffe@gmail.com>
2023-02-13 16:35:02 +0000

    minor updates

--- src/components/libraryContainer/LibraryContainer.js
+++ src/components/libraryContainer/LibraryContainer.js
@@ -3,27 +3,12 @@ import React from "react";
 import './LibraryContainer.css';
 
 const LibraryContainer = () => {
-  // if(!fetchError){
     return (
       <div className="LibraryContainer">
-        {/* {books.map((book, index) => (
-          <LibraryCard key={index} book={book} />
-        ))} */}
         <p>Library</p>
       </div>
-    );
-  // } else if (fetchError) {
-  //   return <ErrorInFetchBooks />
-  // }
-};
+    )
+}
 
-
-// const ErrorInFetchBooks = () => {
-//   return(
-//     <div>
-//       <h1>Error in Fetch Request</h1>
-//     </div>
-//   );
-// }
 
 export default LibraryContainer;
