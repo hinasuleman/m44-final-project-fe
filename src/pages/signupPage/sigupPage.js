4a322406b61242c1d408cdfb023fc36b54cf7f7f
hsnadrs <hsnadrs@yahoo.com>
2023-02-13 19:18:58 +0000

    signuhandeler

--- src/pages/signupPage/sigupPage.js
+++ src/pages/signupPage/sigupPage.js
@@ -6,7 +6,7 @@ export default function SignupPage() {
     const [surname, setSurname] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [confirmEmail, setConfirmEmail] = useState("")
+    const [confirmEmail, setConfirmEmail] = useState("false")
 
     function changehandler(event, setter, state) {
         setter(event.target.value)
@@ -14,14 +14,38 @@ export default function SignupPage() {
         
     }
 
+   
+   
 
-
-  function submitHandler(event) {
+  async function submitHandler(event) {
     event.preventDefault()
     console.log("signup functionality");
+    //check if email is wrtten correctly before submit
+    if (email != confirmEmail){
+        setConfirmEmail (false); 
+    
+       }else{
+        //check not empty 
+        if (email == "" && confirmEmail == "" || firstName == "" || surname == "" ||  password == ""){
+        setConfirmEmail (true);
+       }
+       }
+    if (confirmEmail === true ){
+        await addUser(firstName,surname, email,password,setter);
+    } else {
+    //if wronge email and no field is blank return and reinput
+    if (){
+        setConfirmEmail (false)
+    }
+       
+        
+    }
+    
+    
   }
   return (
     <div>
+        {confirmEmail && <H3> Email not matched or field empty please input again</H3>}
       <form onSubmit={(event) => submitHandler(event)}>
         <input placeholder="first name" onChange={(event) => changehandler(event,setFirstName,firstName)} />
         <input placeholder= "surname" onChange={(event) => changehandler(event,setSurname,surname)} />
