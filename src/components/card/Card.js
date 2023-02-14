080f02dacc46e36bc048b6639589ee9aced0095f
Chris Sutcliffe <cjsutcliffe@gmail.com>
2023-02-14 14:07:50 +0000

    add to library working

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,23 +1,14 @@
 import React from "react";
+import { postToLibrary } from "../../utilities/utilities";
 import './Card.css';
 
 const Card = ({ book }) => {
-  const postToLibrary = (e) => {
-    e.preventDefault();
-    //below code needed to write to DB
-    // const newBook = {
-    //   id: book.id,
-    //   title: book.volumeInfo.title,
-    //   author: book.volumeInfo.authors[0],
-    //   isbn: book.volumeInfo.industryIdentifiers[0].identifier,
-    //   thumbnail: book.volumeInfo.imageLinks.thumbnail,
-    //   description: book.volumeInfo.description,
-    //   category: book.volumeInfo.categories[0],
-    //   selfLink: book.selfLink,
-    //   publishedDate: book.volumeInfo.publishedDate,
-    // };
-    // console.log in lieu of POST fetch request
-
+  const submitHandler1 = () => {
+    // e.preventDefault();
+    const user = {user_ID:12};
+    console.log("click")
+    postToLibrary(book,user);
+    console.log("book added to library");
   };
 
   return (
@@ -27,7 +18,7 @@ const Card = ({ book }) => {
       <br></br>
       <p className="title">{book.volumeInfo.title}</p>
       <p className="author">{book.volumeInfo.authors}</p>
-      <button onClick={postToLibrary}>Add to Library</button>
+      <button onClick={() => (submitHandler1())}>Add to Library</button>
       <button >Add to Wishlist</button>
     </div>
 
@@ -35,7 +26,3 @@ const Card = ({ book }) => {
 };
 
 export default Card;
-
-//{book.volumeInfo.imageLinks.thumbnail ? <img src={book.volumeInfo.imageLinks.thumbnail} alt=""/> : <p>No Image</p>}
-
-//<img src="../../images/blankCover.jpg" alt=""/>
--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -10,3 +10,36 @@ export const fetchBooks = async (searchTerm, searchType, setBooks, setFetchError
   }
   
 };
+
+export const postToLibrary = async (book,user) => {
+  try {
+    console.log(book.volumeInfo)
+    const blankImage = require("../images/blankCover.jpg");
+    let category  = "No Category Information";
+    console.log(category);
+    if (!book.volumeInfo.description) {book.volumeInfo.description = "No Description Information"};  
+    if (!book.volumeInfo.publishedDate) {book.volumeInfo.publishedDate = "No Publication Date Information"};  
+    if (!book.volumeInfo.imageLinks.thumbnail) {book.volumeInfo.imageLinks.thumbnail = blankImage};  
+    const response = await fetch(`${process.env.REACT_APP_REST_API_URL}addBook`, {
+          method:"POST",
+          headers: {"Content-Type" : "application/json"}, //token not need at the moment
+          body: JSON.stringify({
+              user_ID: user.user_ID,
+              google_ID: book.id,
+              title: book.volumeInfo.title,
+              author: book.volumeInfo.authors[0],
+              isbn: book.volumeInfo.industryIdentifiers[0].identifier,
+              thumbnail: book.volumeInfo.imageLinks.thumbnail,
+              description: book.volumeInfo.description,
+              category: category,
+              selfLink: book.selfLink,
+              publishedDate: book.volumeInfo.publishedDate 
+          }
+          )
+      })
+      const data = await response.json();
+      console.log(data);
+  } catch (error) {
+      console.log(error)
+  }
+}
