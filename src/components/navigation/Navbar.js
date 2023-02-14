aeaf8ab3a10d6c2ed70807c2408a2ae6a04d94e4
coopsie <aislingkcooper@hotmail.com>
2023-02-14 18:32:25 +0000

    fixed logo issue

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-// import logo from "../../../src/images/logo/logo_outline.png";
+import logo from "../../images/logo/logo_outline_colour.png";
 import book from "../../../src/images/icons/book.png";
 import heart from "../../../src/images/icons/heart.png";
 import user from "../../../src/images/icons/user.png";
@@ -13,22 +13,22 @@ import "./Navbar.css";
 function Navbar () {
 
         return(
-            <nav className="NavbarItems">
-                {/* <NavLink to="/mainpage"><img src={logo} alt="Logo" className="logoImg" /></NavLink>             */}
+            <div className="NavbarItems">
+                <NavLink to="/mainpage"><img src={logo} alt="Logo" className="logoImg" /></NavLink>            
                 <div className="navMenu">
                     <NavLink to="/mainpage"><img src={book} alt="My Library Icon" />My Library</NavLink>
                     
                     <NavLink to="/wishlistpage"><img src={heart} alt="Wishlist Icon" />Wish List</NavLink>
                     
-                    {/* <li><NavLink to="/"><img src={peoplearrows} alt="Share Icon" className="icon" activeClassName="active"/>Share</NavLink> */}
-                    {/* <img src={peoplearrows} alt="Share Books Icon" />Share</li> */}
+                    {/* <li><NavLink to="/"><img src={peoplearrows} alt="Share Icon" />Share</NavLink> */}
+                    <li><img src={peoplearrows} alt="Share Books Icon" />Share</li>
                     
-                    {/* <li><NavLink to="/"><img src={group} alt="Community Icon" className="icon" activeClassName="active"/>Community</NavLink> */}
-                    {/* <img src={group} alt="Community Icon" />Community</li> */}
+                    {/* <li><NavLink to="/"><img src={group} alt="Community Icon" />Community</NavLink> */}
+                    <li><img src={group} alt="Community Icon" />Community</li>
                     
                     <NavLink to="/userprofilepage"><img src={user} alt="Userprofile Icon" />Profile</NavLink>
                 </div>
-            </nav>
+            </div>
     );
 }
 
