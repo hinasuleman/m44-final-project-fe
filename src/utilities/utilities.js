71d8e5880cf57cf64fcb2113b112457046d29b0e
hinasuleman <119439264+hinasuleman@users.noreply.github.com>
2023-02-14 21:53:01 +0000

    delete user functionality completed

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -10,3 +10,27 @@ export const fetchBooks = async (searchTerm, searchType, setBooks, setFetchError
   }
   
 };
+
+
+export const deleteUser = async (userName,email,password) => {
+  try {
+    console.log("deleteuser")
+    console.log(userName,email,password)
+    const response = await fetch(
+      `${process.env.REACT_APP_REST_API_URL}deleteUser`,{
+        method:"DELETE",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          userName: userName,
+          email: email,
+          password: password
+        })
+      })
+    const data = await response.json()
+    console.log(data)
+    
+  } catch (error) {
+    console.log(error);
+  }
+  
+};
