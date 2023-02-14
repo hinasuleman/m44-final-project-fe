ec33a0ba85b6a1e1c19c7be6a295af0bcdf034d5
coopsie <aislingkcooper@hotmail.com>
2023-02-14 14:00:50 +0000

    deleted unused logos and uploaded new ones. I installed an icon package but then deleted it which is why a change is showing in package-lock.json but no actual change has occured

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,11 +3,13 @@ import { NavLink } from "react-router-dom";
 import logo from "../../images/logo/logo_1.png";
 import "./Navbar.css";
 
+
+
 function Navbar () {
 
         return(
             <nav className="NavbarItems">
-                <a href="/mainpage"><img src={logo} alt="Logo" className="logoImg" /></a>            
+                <NavLink to="/mainpage"><img src={logo} alt="Logo" className="logoImg" /></NavLink>            
                 <ul className="navMenu">
                     <li><NavLink to="/mainpage" activeClassName="active">My Library</NavLink></li>
                     
