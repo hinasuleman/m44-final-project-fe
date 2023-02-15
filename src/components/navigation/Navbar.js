5db06c25fa7b5477a6682bbac5ef2b262084067c
coopsie <aislingkcooper@hotmail.com>
2023-02-15 18:05:46 +0000

    fixed responsiveness of navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -6,6 +6,7 @@ import heart from "../../../src/images/icons/heart.png";
 import user from "../../../src/images/icons/user.png";
 import peoplearrows from "../../../src/images/icons/people-arrows.png";
 import group from "../../../src/images/icons/people-group.png";
+import logout from "../../../src/images/icons/right-from-bracket.png";
 import "./Navbar.css";
 
 function Navbar () {
@@ -19,29 +20,28 @@ function Navbar () {
 
                     <NavLink to="/wishlistpage"><img src={heart} alt="Wishlist Icon" />Wish List</NavLink>
                     
-                    {/* <NavLink to="/"><img src={peoplearrows} alt="Share Icon" />Share</NavLink> */}
-                    <li><img src={peoplearrows} alt="Share Books Icon" />Share</li>
+                    <NavLink to="/"><img src={peoplearrows} alt="Share Icon" />Share</NavLink>
                     
-                    {/* <NavLink to="/"><img src={group} alt="Community Icon" />Community</NavLink> */}
-                    <li><img src={group} alt="Community Icon" />Community</li>
+                    <NavLink to="/"><img src={group} alt="Community Icon" />Community</NavLink>
                     
                     <NavLink to="/userprofilepage"><img src={user} alt="Userprofile Icon" />Profile</NavLink>
+
+                    <NavLink to="/landingpage"><img src={logout} alt="Logout Icon" />Log Out</NavLink>
                 </div>
 
                 <div className="navbarItemsSmall">
-                    {/* <NavLink to="/mainpage"><img src={logo} alt="Logo" className="logoImg" /></NavLink> */}
                 
                     <NavLink to="/mainpage"><img src={book} alt="My Library Icon" /></NavLink>
 
                     <NavLink to="/wishlistpage"><img src={heart} alt="Wishlist Icon" /></NavLink>
                                 
-                    {/* <NavLink to="/"><img src={peoplearrows} alt="Share Icon" />Share</NavLink> */}
-                    {/* <li><img src={peoplearrows} alt="Share Books Icon" /></li> */}
+                    <NavLink to="/"><img src={peoplearrows} alt="Share Icon" /></NavLink>
                                 
-                    {/* <NavLink to="/"><img src={group} alt="Community Icon" />Community</NavLink> */}
-                    {/* <li><img src={group} alt="Community Icon" /></li> */}
+                    <NavLink to="/"><img src={group} alt="Community Icon" /></NavLink>
                                 
                     <NavLink to="/userprofilepage"><img src={user} alt="Userprofile Icon" /></NavLink>
+
+                    <NavLink to="/landingpage"><img src={logout} alt="Logout Icon" /></NavLink>
                     </div>
                 </div>
         );
