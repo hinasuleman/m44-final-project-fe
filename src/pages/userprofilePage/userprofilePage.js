70a7f22474e2ac1f51a9b414e65a97cf28514baa
hinasuleman <119439264+hinasuleman@users.noreply.github.com>
2023-02-15 11:40:43 +0000

    updated userprofilepage

--- src/pages/userprofilePage/userprofilePage.js
+++ src/pages/userprofilePage/userprofilePage.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./userprofilePage.css";
 import DeleteUserModal from '../../components/modals/deleteusermodal/DeleteUserModal'
 
-export default function UserprofilePage() {
+  function UserprofilePage() {
   const [currentEmail, setCurrentEmail] = useState("")
   const [newEmail, setNewEmail] = useState("")
   const [currentPassword, setCurrentPassword] = useState("")
@@ -52,3 +52,6 @@ export default function UserprofilePage() {
     </div>
   );
 }
+
+export default UserprofilePage;
+
