07372899cba885f1d23a0b1b741269c8584ba389
Enoch Jongwe <enojo3@outlook.com>
2023-02-16 17:29:45 +0000

    footer needs compiling

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ import UserprofilePage from "./pages/userprofilePage/userprofilePage";
 import Navbar from './components/navigation/Navbar';
 import WishlistPage from "./pages/wishlistPage/wishlistPage";
 import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
 
 
 function App() {
   const [books, setBooks] = useState([]);
   const [fetchError, setFetchError] = useState(false);
-  const [user] = useState([]); //need to sort setUser
+  const [user, setUser] = useState([]); // fix: add setUser
   const [bookList, setBookList] = useState(listBooks());
   const [wishList, setWishList] = useState(wishListBooks());
 
@@ -46,44 +45,23 @@ function App() {
               } />
           </Routes>
         </div>
+        <Footer setUser={setUser}/> {/* fix: pass setUser as a prop */}
     </BrowserRouter>
   );
 }
 
-function App() {
-	return (
-	  <Header />
-	);
+function Footer ({ setUser }) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
-
-function App() {
-	return ( 
-	<BrowserRouter>
-		<Footer />
-		  <div>
-			<Routes>
-			  <Route exact path="/" element={<LandingPage setter={setUser}/>} />
-			  <Route exact path="/mainpage" element={
-					<MainPage 
-					  setBooks={setBooks}
-					  setBookList={setBookList}
-					  bookList={bookList}
-					  fetchError={fetchError}
-					  setFetchError={setFetchError}
-					  books={books}
-					  user={user}
-					/>
-				 }/>
-			  <Route exact path="/wishlistpage" element={
-					<WishlistPage
-					setWishList={setWishList}
-					WishList={wishList}
-					 />
-				} />
-			</Routes>
-		  </div>
-	</BrowserRouter>
-	);
-}
-
+// npm install --save styled-components 
 export default App;
