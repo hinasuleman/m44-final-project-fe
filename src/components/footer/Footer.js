0cd0d671b4eb3b4ab13f62a60e3236641be18df8
Enoch Jongwe <enojo3@outlook.com>
2023-02-16 15:47:17 +0000

    header and footer in just need to style it to fit in with the design

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyles";
+
+const Footer = () => {
+return (
+	<Box>
+	<h1 style={{ color: "green",
+				textAlign: "center",
+				marginTop: "-50px" }}>
+		BiblioTech
+	</h1>
+	<Container>
+		<Row>
+		<Column>
+			<Heading>About Us</Heading>
+			<FooterLink href="#">Books</FooterLink>
+			<FooterLink href="#">Wishlist</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Services</Heading>
+			<FooterLink href="#">Main Page</FooterLink>
+			<FooterLink href="#">UserprofilePage</FooterLink>
+			<FooterLink href="#">Wishlist</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Social Media</Heading>
+			<FooterLink href="#">
+			<i className="fab fa-facebook-f">
+				<span style={{ marginLeft: "10px" }}>
+				Facebook
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-instagram">
+				<span style={{ marginLeft: "10px" }}>
+				Instagram
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-twitter">
+				<span style={{ marginLeft: "10px" }}>
+				Twitter
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-youtube">
+				<span style={{ marginLeft: "10px" }}>
+				Youtube
+				</span>
+			</i>
+			</FooterLink>
+		</Column>
+		</Row>
+	</Container>
+	</Box>
+);
+};
+export default Footer;
